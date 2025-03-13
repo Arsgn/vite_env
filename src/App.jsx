58384cfd62fb3e1575168963d2bccb6f80873b69
tytytytyiew <konurbaevbarsbek@gmail.com>
2025-03-13 20:42:18 +0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ const VITE_ID = import.meta.env.VITE_ID;
 console.log(VITE_ID);
 
 const App = () => {
-  return <div>{VITE_ID} App</div>;
+  return <div>{VITE_ID} Appq</div>;
 };
 
 export default App;
